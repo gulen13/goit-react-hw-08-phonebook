7a3e818df1758322c379823bf676b8b78a26c653
gulen13 <114436484+gulen13@users.noreply.github.com>
2023-06-29 19:25:51 +0000

    Deploying to gh-pages from @ gulen13/goit-react-hw-08-phonebook@d0e2723d8954af84bb71500ed1e029b0bb380382 🚀

---
+++ static/js/415.d7d1e2ea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_07_phonebook=self.webpackChunkgoit_react_hw_07_phonebook||[]).push([[415],{5415:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var o=t(4554),r=t(184);function c(){return(0,r.jsx)(o.Z,{sx:{display:"flex",justifyContent:"center",pt:10},children:(0,r.jsx)("h1",{children:"Phone book manager welcome page"})})}}}]);
+//# sourceMappingURL=415.d7d1e2ea.chunk.js.map
